a4a3df99ab321d9ed4acc6146fe11ae194f129ec
Edimilson Borges <104403198+EdimilsonBorges@users.noreply.github.com>
2023-09-05 09:23:03 -0300

    first commit

---
+++ src/Usuario.java
@@ -0,0 +1,19 @@
+public class Usuario {
+
+    public static void main(String[] args) {
+        Iphone iphone = new Iphone();
+        
+        iphone.ligar();
+        iphone.atender();
+        iphone.iniciarCorreioVoz();
+
+        iphone.exibirPagina();
+        iphone.adicionarNovaAba();
+        iphone.atualizarPagina();
+
+        iphone.selecionarMusica();
+        iphone.tocar();
+        iphone.pausar();
+    }
+
+}
